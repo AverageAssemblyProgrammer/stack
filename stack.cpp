460d9219e2952a183c6fa554d5cd4217b3c3efae
AverageAssemblyProgrammer <AverageAssembly8664@gmail.com>
2022-04-24 20:44:17 +0530

    implemented `read` subcommand

--- stack.cpp
+++ stack.cpp
@@ -4,7 +4,7 @@
 #include <cstring>
 #include <filesystem>
 #include <fstream>
-#include <cassert>
+#include <vector>
 
 bool ends_with(const std::string &filename, const std::string &ext)
 {
@@ -114,12 +114,22 @@ void interpret_program(std::stack<int> stack, std::string ip){
 	stack.pop();
 	stack.push(a);
 	stack.push(a);
-      }
+      }	
       else{
         std::cout << "the `dup` intrinsic needs at least 1 element on the stack" << "\n";
       }
     }
-
+    else if (ip == "drop-all"){
+      if (stack.size() >= 1){
+	while (stack.empty()==0){
+	  stack.pop();
+	}
+      }
+      else {
+        std::cout << "the `drop all` intrinsic needs at least 1 element on the stack" << "\n";
+      }      
+    }
+      
     // TODO: implement over intrinsic
     else if (ip == "over"){
       if (stack.size() >= 2){
@@ -168,9 +178,140 @@ void interpret_program(std::stack<int> stack, std::string ip){
   }
 }
 
-void interpret_read_program(std::stack<int> stack, std::string ip){
-  if (is_digits(ip)==1){
-    stack.push(stoi(ip));
+void interpret_read_program(std::stack<int> stack, std::vector<std::string> ips){
+  for (std::string ip : ips) {
+    if (is_digits(ip) == 1){
+      stack.push(stoi(ip));
+    }
+
+    // printing the first element of the stack 
+    else if (ip == "print"){
+      if (stack.size() == 0){
+	std::cout << "warning: stack is empty" << std::endl;
+      }
+      else {
+        std::cout << stack.top() << "\n";
+        stack.pop();
+      }
+    }
+
+    else if (ip == "+"){
+      if (stack.size() >= 2) {
+	int a = stack.top();
+	stack.pop();
+	int b = stack.top();
+	stack.pop();
+	int c = b + a;
+	stack.push(c);
+      }
+      else {
+        std::cout << "`+` operation needs at least 2 elements on the stack" << "\n";
+      }
+    }
+
+    else if (ip == "-"){
+      if (stack.size() >= 2){
+	int a = stack.top();
+	stack.pop();
+	int b = stack.top();
+	stack.pop();
+	int c = b - a;
+	stack.push(c);
+      }
+      else {
+        std::cout << "`-` operation needs at least 2 elements on the stack" << "\n";
+      }
+    }
+
+    else if (ip == "drop"){
+      if (stack.size() >= 1){
+        stack.pop();
+      }
+      else {
+        std::cout << "the `drop` intrinsic needs at least 1 element on the stack" << "\n";
+      }
+    }
+
+    else if (ip == "swap"){
+      if (stack.size() >= 2){
+	int a = stack.top();
+	stack.pop();
+	int b = stack.top();
+	stack.pop();
+	stack.push(a);
+	stack.push(b);
+      }
+      else {
+        std::cout << "the `swap` intrinsic needs at least 2 elements on the stack" << "\n";
+      }
+    }
+
+    else if (ip == "dup"){
+      if (stack.size() >= 1){
+	int a = stack.top();
+	stack.pop();
+	stack.push(a);
+	stack.push(a);
+      }	
+      else{
+        std::cout << "the `dup` intrinsic needs at least 1 element on the stack" << "\n";
+      }
+    }
+    else if (ip == "drop-all"){
+      if (stack.size() >= 1){
+	while (stack.empty()==0){
+	  stack.pop();
+	}
+      }
+      else {
+        std::cout << "the `drop all` intrinsic needs at least 1 element on the stack" << "\n";
+      }      
+    }
+      
+    // TODO: implement over intrinsic
+    else if (ip == "over"){
+      if (stack.size() >= 2){
+	printf("Not implemented yet: `over`");
+	exit(1);
+      }
+      else{
+	std::cout << "the `over` intrinsic needs at least 2 elements on the stack" << "\n";
+      }
+    }
+
+    else if (ip == "rot"){
+      if (stack.size() >= 3) {
+	int a = stack.top();
+	stack.pop();
+	int b = stack.top();
+	stack.pop();
+	int c = stack.top();
+	stack.pop();
+	stack.push(a);
+	stack.push(b);
+	stack.push(c);
+      }
+      else {
+	std::cout << "the `rot` intrinsic needs at least 3 elements on the stack" << "\n";
+      }
+    }
+
+    else if (ip == "???") {
+      if (stack.empty()){
+	std::cout << "There is nothing on the stack currently" << "\n";
+      }
+      else{
+	PrintStack(stack);
+      }
+    }
+    
+    else if (ip == "exit"){
+      exit(0);
+    }
+    
+    else {
+      std::cout << "not implemented: " << ip << std::endl;
+    }
   }
 }
 
@@ -201,16 +342,14 @@ int main(int argc, char* argv[]){
       printf("ERROR: file does not exist");
       exit(1);
     }
-    
     if (ends_with(filepath, ".stack")){
-      printf("Not implemented yet");
-      exit(1);
-      std::fstream file(filepath);
-      std::string text;
-      while (getline(file, text)){
-	// FIXME
-	interpret_read_program(stack, text);
+      std::ifstream file(filepath);
+      std::vector<std::string> words;
+      std::string foundWord;
+      while (file >> foundWord) {
+	words.push_back(foundWord);
       }
+      interpret_read_program(stack, words);
       file.close();
     }
     else {
